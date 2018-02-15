41990aaceddb3014b16bbd58ee613fbad0a80f0e
xuj <xuj2017@outlook.com>
2018-02-15 18:40:36 +0800

    player

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -3,7 +3,25 @@ import * as types from './mutation-types';
 const matutations = {
     [types.SET_SINGER](state, singer) {
         state.singer = singer
-      },
+    },
+    [types.SET_PLAYING_STATE](state, flag) {
+        state.playing = flag
+    },
+    [types.SET_FULL_SCREEN](state, flag) {
+        state.fullScreen = flag
+    },
+    [types.SET_PLAYLIST](state, list) {
+        state.playList = list
+    },
+    [types.SET_SEQUENCE_LIST](state, list) {
+        state.seqeuenceList = list
+    },
+    [types.SET_PLAY_MODE](state, mode) {
+        state.mode = mode;
+    },
+    [types.SET_CURRENT_INDEX](state, index) {
+        state.currentIndex = index
+    }
 }
 
 
