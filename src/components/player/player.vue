ad7f1e1c473e97d2ab8ffae45802a8e5559bded3
xujun <xuj2017@outlook.com>
2018-03-06 21:04:46 +0800

    playing

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -1,6 +1,11 @@
 <template>
-  <div class="player" v-show="playlist">
-    <transition name="normal">
+  <div class="player" v-show="playlist.length>0">
+    <transition name="normal"
+      @enter="enter"
+      @after-enter="afterEnter"
+      @leave="leave"
+      @after-leave="afterLeave"
+    >
       <div class="normal-player" v-show="fullScreen">
         <div class="background">
           <img height="100%" width="100%" :src="currentSong.image">
@@ -14,8 +19,8 @@
         </div>
         <div class="middle">
           <div class="middle-l">
-            <div class="cd-wrapper">
-              <div class="cd">
+            <div class="cd-wrapper" ref="cdWrapper">
+              <div class="cd" :class="cdClass">
                 <img class="image" :src="currentSong.image">
               </div>
             </div>
@@ -30,7 +35,7 @@
               <i class="icon-prev"></i>
             </div>
             <div class="icon i-center">
-              <i class="icon-play"></i>
+              <i @click="togglePlaying" :class="playIcon"></i>
             </div>
             <div class="icon i-left">
               <i class="icon-next"></i>
@@ -45,26 +50,44 @@
     <transition name="mini">
       <div class="mini-player"  v-show="!fullScreen" @click="open">
         <div class="icon">
-          <img width="40" height="40" :src="currentSong.image">
+          <img :class="cdClass" width="40" height="40" :src="currentSong.image">
         </div>
         <div class="text">
           <h2 class="name" v-html="currentSong.name"></h2>
           <p class="desc" v-html="currentSong.singer"></p>
         </div>
-        <div class="control"></div>
+        <div class="control">
+          <i @click.stop="togglePlaying" :class="miniIcon"></i>
+        </div>
         <div class="control">
           <i class="icon-playlist"></i>
         </div>
       </div>
     </transition>
+    <audio ref="audio" :src="currentSong.url"></audio>
   </div>
 </template>
 
 <script>
 import { mapGetters, mapMutations } from "vuex";
+import {prefixStyle} from 'common/js/dom'
+import animations from 'create-keyframe-animation';
+
+const transform = prefixStyle('transform')
+const transitionDuration = prefixStyle('transitionDuration')
+
 export default {
   computed: {
-    ...mapGetters(["fullScreen", "playlist", "currentSong"])
+    playIcon(){
+      return this.playing?'icon-pause':'icon-play';
+    },
+    miniIcon(){
+      return this.playing?'icon-pause-mini':'icon-play-mini';
+    },
+    cdClass(){
+      return this.playing?'play':'';
+    },
+    ...mapGetters(["fullScreen", "playlist", "currentSong","playing"])
   },
   methods: {
     back() {
@@ -73,9 +96,80 @@ export default {
     open() {
       this.setFullScreen(true);
     },
+    enter(el,done){
+      const{x,y,scale} = this.getPosAndScale();
+
+      let animation = {
+        0:{
+          transform:`translate3d(${x}px,${y}px,0) scale(${scale})`
+        },
+        60:{
+          transform:`translate3d(0px,0px,0) scale(1.1)`
+        },
+        0:{
+          transform:`translate3d(0px,0px,0) scale(1)`
+        },
+      }
+      animations.registerAnimation({
+        name:'move',
+        animation,
+        presets:{
+          duration:400,
+          easing:'linear'
+        }
+      })
+
+      animations.runAnimation(this.$refs.cdWrapper,'move',done);
+    },
+    afterEnter(){
+      animations.unregisterAnimation('move');
+      this.$refs.cdWrapper.style.animation = '';
+    },
+    leave(el,done){
+      this.$refs.cdWrapper.style.transition = 'all 0.4s'
+      const{x,y,scale} = this.getPosAndScale();
+      this.$refs.cdWrapper.style[transform] = `translate3d(${x}px,${y}px,0) scale(${scale})`;
+      this.$refs.cdWrapper.addEventListener('transitionend',done);
+    },
+    afterLeave(){
+      this.$refs.cdWrapper.style.transition='';
+      this.$refs.cdWrapper.style[transform] = '';
+    },
+    getPosAndScale(){
+      const targetWidth = 40;
+      const paddingLeft = 40;
+      const paddingBottom = 30;
+      const paddingTop = 80;
+      const width = window.innerWidth*0.8;
+      const scale = targetWidth/width;
+      const x = -(window.innerWidth/2-paddingLeft);
+      const y = window.innerHeight - paddingBottom - width/2;
+      return{
+        x,y,scale
+      }
+
+    },
+    togglePlaying(e){
+      this.setPlayingState(!this.playing);
+    },
     ...mapMutations({
-      setFullScreen: "SET_FULL_SCREEN"
+      setFullScreen: "SET_FULL_SCREEN",
+      setPlayingState:'SET_PLAYING_STATE'
     })
+  },
+  watch:{
+    currentSong(){
+      this.$nextTick(()=>{
+        this.$refs.audio.play();
+      })
+      
+    },
+    playing(newPlaying){
+      const audio = this.$refs.audio;
+      this.$nextTick(()=>{
+        newPlaying ? audio.play():audio.pause();
+      })
+    }
   }
 };
 </script>
@@ -316,6 +410,7 @@ export default {
     width: 100%;
     height: 60px;
     background: $color-highlight-background;
+    // display: none;
     &.mini-enter-active,
     &.mini-leave-active {
       transition: all 0.4s;
