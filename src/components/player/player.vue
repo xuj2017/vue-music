7bc86943a53c342c515b94dafeeb4e8cc2706599
xuj <xuj2017@outlook.com>
2018-03-23 23:05:26 +0800

    palyer

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -17,16 +17,34 @@
           <div class="title" v-html="currentSong.name"></div>
           <h2 class="subtitle" v-html="currentSong.singer"></h2>
         </div>
-        <div class="middle">
-          <div class="middle-l">
+        <div class="middle" 
+        @touchstart.prevent="middleTouchStart"
+        @touchmove.prevent="middleTouchMove"
+        @touchend="middleTouchEnd"
+        >
+          <div class="middle-l" ref="middleL">
             <div class="cd-wrapper" ref="cdWrapper">
               <div class="cd" :class="cdClass">
                 <img class="image" :src="currentSong.image">
               </div>
             </div>
           </div>
+          <scroll class="middle-r" ref="lyricList" :data="currentLyric && currentLyric.lines"> 
+            <div class="lyric-wrapper">
+               <div v-if="currentLyric">
+                <p ref="lyricLine"
+                   class="text"
+                    :class="{'current':currentLineNum === index}"
+                   v-for="(line,index) in currentLyric.lines">{{line.txt}}</p>
+              </div>
+            </div>
+          </scroll>
         </div>
         <div class="bottom">
+          <div class="dot-wrapper">
+            <div class="dot" :class="{'active':currentShow == 'cd'}"></div>
+            <div class="dot" :class="{'active':currentShow == 'lyric'}"></div>
+          </div>
           <div class="progress-wrapper">
             <span class="time time-l">{{format(currentTime)}}</span>
             <div class="progress-bar-wrapper">
@@ -86,7 +104,8 @@ import ProgressCircle from 'base/progress-circle/progress-circle';
 
 import {playMode} from 'common/js/config';
 import {shuffle} from 'common/js/util.js';
-import Lyric from 'lyric-parser'
+import Lyric from 'lyric-parser';
+import Scroll from 'base/scroll/scroll';
 
 const transform = prefixStyle('transform')
 const transitionDuration = prefixStyle('transitionDuration')
@@ -97,7 +116,9 @@ export default {
       songReady:false,//用于防止快速点击
       currentTime:0,
       radius:32,
-      currentLyric:null
+      currentLyric:null,
+      currentLineNum:0,
+      currentShow:'cd'
     }
   },
   computed: {
@@ -120,6 +141,9 @@ export default {
       return this.mode == playMode.sequence ? "icon-sequence" : this.mode == playMode.loop? 'icon-loop':'icon-random'; 
     },
     ...mapGetters(["fullScreen", "playlist", "currentSong","playing","currentIndex","mode","sequenceList"])
+  },
+  created(){
+    this.touch = {};
   },
   methods: {
     back() {
@@ -276,9 +300,85 @@ export default {
     },
     getLyric(){
       this.currentSong.getLyric().then((lyric)=>{
-        this.currentLyric = new Lyric(lyric);
+        this.currentLyric = new Lyric(lyric,this.handleLyric);
+        if(this.playing){
+          this.currentLyric.play()
+        }
         console.log(this.currentLyric);
       })
+    },
+    handleLyric({lineNum,txt}){
+        this.currentLineNum = lineNum;
+        if(lineNum>5){
+          let lineEl = this.$refs.lyricLine[lineNum-5]
+          this.$refs.lyricList.scrollToElement(lineEl,1000)
+        }else{
+          this.$refs.lyricList.scrollTo(0,0,1000);
+        }
+    },
+    middleTouchStart(e){
+      this.touch.initiated = true;
+       // 用来判断是否是一次移动
+        this.touch.moved = false
+        const touch = e.touches[0]
+        this.touch.startX = touch.pageX
+        this.touch.startY = touch.pageY
+    },
+    middleTouchMove(e){
+       if (!this.touch.initiated) {
+          return
+        }
+        const touch = e.touches[0]
+        const deltaX = touch.pageX - this.touch.startX
+        console.log(touch.pageX,deltaX)
+        const deltaY = touch.pageY - this.touch.startY
+        if (Math.abs(deltaY) > Math.abs(deltaX)) {
+          return
+        }
+
+        if (!this.touch.moved) {
+          this.touch.moved = true
+        }
+
+        const left = this.currentShow === 'cd' ? 0 : -window.innerWidth
+        const offsetWidth = Math.min(0, Math.max(-window.innerWidth, left + deltaX))
+        this.touch.percent = Math.abs(offsetWidth / window.innerWidth)
+        this.$refs.lyricList.$el.style[transform] = `translate3d(${offsetWidth}px,0,0)`
+        this.$refs.lyricList.$el.style[transitionDuration] = 0
+        this.$refs.middleL.style.opacity = 1 - this.touch.percent
+        this.$refs.middleL.style[transitionDuration] = 0
+    },
+    middleTouchEnd(){
+      if (!this.touch.moved) {
+          return
+        }
+        let offsetWidth
+        let opacity
+        if (this.currentShow === 'cd') {
+          if (this.touch.percent > 0.1) {
+            offsetWidth = -window.innerWidth
+            opacity = 0
+            this.currentShow = 'lyric'
+          } else {
+            offsetWidth = 0
+            opacity = 1
+          }
+        } else {
+          if (this.touch.percent < 0.9) {
+            offsetWidth = 0
+            this.currentShow = 'cd'
+            opacity = 1
+          } else {
+            offsetWidth = -window.innerWidth
+            opacity = 0
+          }
+        }
+        const time = 300
+        this.$refs.lyricList.$el.style[transform] = `translate3d(${offsetWidth}px,0,0)`
+        this.$refs.lyricList.$el.style[transitionDuration] = `${time}ms`
+        this.$refs.middleL.style.opacity = opacity
+        this.$refs.middleL.style[transitionDuration] = `${time}ms`
+        this.touch.initiated = false
     },
     ...mapMutations({
       setFullScreen: "SET_FULL_SCREEN",
@@ -309,7 +409,8 @@ export default {
   },
   components:{
     ProgressBar,
-    ProgressCircle
+    ProgressCircle,
+    Scroll
   }
 };
 </script>
@@ -439,7 +540,7 @@ export default {
             line-height: 32px;
             color: $color-text-l;
             font-size: $font-size-medium;
-            .current {
+            &.current {
               color: $color-text;
             }
           }
@@ -454,7 +555,8 @@ export default {
         text-align: center;
         font-size: 0;
         .dot {
-          text-align: middle;
+          display: inline-block;
+          vertical-align: middle;
           margin: 0 4px;
           width: 8px;
           height: 8px;
