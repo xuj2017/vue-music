bc24fbbcf7c46351c3bd7c92471f99cc7b9bead8
xuj <xuj2017@outlook.com>
2018-03-27 23:16:48 +0800

    search

--- src/components/search/search.vue
+++ src/components/search/search.vue
@@ -1,17 +1,128 @@
 <template>
-  <div>
-      搜索页面
+  <div class="search">
+      <div class="search-box-wrapper">
+        <search-box ref="searchBox" @query="onQueryChange"></search-box>
+      </div>
+      <div class="shortcut-wrapper"  v-if="!query">
+        <div class="shortcut">
+          <div class="hot-key">
+            <h1 class="title">热门搜索</h1>
+            <ul>
+              <li class="item" @click="addQuery(item.k)" v-for="(item,index) in hotkey">
+              <span>{{item.k}}</span>
+            </li>
+            </ul>
+            
+          </div>
+        </div>
+      </div>
+      <div class="search-result" v-show="query">
+        <suggest :query="query"></suggest>
+      </div>
   </div>
 </template>
 
 <script>
+import SearchBox from "base/search-box/search-box";
+import { getHotKey } from "api/search";
+import { ERR_OK } from "api/config";
+import Suggest from 'components/suggest/suggest'
 export default {
-  
-}
+  data() {
+    return {
+      hotkey: [],
+      query:''
+    };
+  },
+  components: {
+    SearchBox,
+    Suggest
+  },
+  created() {
+    this._getHotKey();
+  },
+  methods: {
+    _getHotKey() {
+      getHotKey().then(res => {
+        if (res.code == ERR_OK) {
+          console.log(res.data);
+          this.hotkey = res.data.hotkey.slice(0, 10);
+        }
+      });
+    },
+    addQuery(query){
+      this.$refs.searchBox.setQuery(query);
+    },
+    onQueryChange(query){
+      this.query = query
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
+@import "~common/style/variable";
+@import "~common/style/mixin";
 
+.search {
+  .search-box-wrapper {
+    margin: 20px;
+  }
+  .shortcut-wrapper {
+    position: fixed;
+    top: 178px;
+    bottom: 0;
+    width: 100%;
+    .shortcut {
+      height: 100%;
+      overflow: hidden;
+      .hot-key {
+        margin: 0 20px 20px 20px;
+        .title {
+          margin-bottom: 20px;
+          font-size: $font-size-medium;
+          color: $color-text-l;
+        }
+        .item {
+          display: inline-block;
+          padding: 5px 10px;
+          margin: 0 20px 10px 0;
+          border-radius: 6px;
+          background: $color-highlight-background;
+          font-size: $font-size-medium;
+          color: $color-text-d;
+        }
+      }
+      .search-history {
+        position: relative;
+        margin: 0 20px;
+        .title {
+          display: flex;
+          align-items: center;
+          height: 40px;
+          font-size: $font-size-medium;
+          color: $color-text-l;
+          .text {
+            flex: 1;
+          }
+          .clear {
+            @include extend-click();
+            .icon-clear {
+              font-size: $font-size-medium;
+              color: $color-text-d;
+            }
+          }
+        }
+      }
+    }
+    .search-result {
+      position: fixed;
+      width: 100%;
+      top: 178px;
+      bottom: 0;
+    }
+  }
+}
 </style>
 
 
